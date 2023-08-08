022d2a47ba37c394c8dd513d7b4b3c57a8fa7643
fierylion <mawalladaniel2018@gmail.com>
2023-08-08 12:24:40 +0300

    second commit

--- src/components/HomePageSection/Section3.jsx
+++ src/components/HomePageSection/Section3.jsx
@@ -1,17 +1,102 @@
-import React from 'react'
-import img1 from '../../assets/collection/img1.jpg'
-import img2 from '../../assets/collection/img2.jpg'
-import img3 from '../../assets/collection/img3.jpg'
-import img4 from '../../assets/collection/img4.jpg'
-import img5 from '../../assets/collection/img5.jpg'
-import img6 from '../../assets/collection/img6.jpg'
-import img7 from '../../assets/collection/img7.jpg'
-import img8 from '../../assets/collection/img8.jpg'
-import img9 from '../../assets/collection/img9.jpg'
+import React, { useEffect, useState } from 'react'
 
-const Section3 = () => {
+const divideToThree = (noEle, arr) => {
+  let sta = 0
+  let en = noEle
+  let result = []
+  while (sta < arr.length) {
+    result.push(arr.slice(sta, en))
+    sta = en
+    en += noEle
+  }
+  return result
+}
+  
+const Section3 = ({imageCarousel}) => {
+  const [noImages, setNoImages] = useState(3)
+  const handleResize = ()=>{
+    const width = window.innerWidth;
+    console.log(width)
+    if (width < 410){
+      setNoImages(1)
+      return
+    }
+    if (width < 769) {
+        setNoImages(2)
+        return
+    }
+    
+
+    setNoImages(3)
+    
+  }
+  useEffect(
+    ()=>{
+      window.addEventListener('resize', handleResize)
+      return ()=> window.removeEventListener('resize', handleResize)
+    },[]
+  )
+  const images =(imageCarousel)? divideToThree(noImages, imageCarousel): []
   return (
-    <div>Section3</div>
+    <section className='my-5'>
+      <div>
+        <div id='demo' className='carousel slide' data-bs-ride='carousel'>
+          {/* Indicators/dots */}
+          <div className='carousel-indicators'>
+          { 
+            images.map((arr, ind)=> <button type='button' data-bs-target='#demo' data-bs-slide-to={ind} className={`${ind===0 && 'active'}`}/> )
+            
+          }
+          </div>
+          {/* The slideshow/carousel */}
+          <div className='carousel-inner'>
+          { images.map((img, ind)=>{
+            return (
+              <div className={`carousel-item ${ind === 0 && 'active'}`}>
+                <div className='row text-center'>
+                { img.map(
+                  (singleImage, ind)=>{
+                    const noImages= img.length;
+                    const imgWidthMap = new Map([
+                      [1, 12], [2, 6], [3, 4]
+                    ])
+
+                   return  (<div className={`col-${imgWidthMap.get(noImages)}`} key={ind}>
+                    <img src={singleImage} alt={`Image ${ind}`} className='card_img' />
+                  </div>)}
+                )
+                  
+                 
+                }
+                </div>
+              </div>
+            )
+          })
+
+
+          }
+          
+          </div>
+          {/* Left and right controls/icons */}
+          <button
+            className='carousel-control-prev'
+            type='button'
+            data-bs-target='#demo'
+            data-bs-slide='prev'
+          >
+            <span className='carousel-control-prev-icon' />
+          </button>
+          <button
+            className='carousel-control-next'
+            type='button'
+            data-bs-target='#demo'
+            data-bs-slide='next'
+          >
+            <span className='carousel-control-next-icon' />
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Section1, Section2, Section3 } from '../components/HomePageSection'
+import imageCarousel from '../components/HomePageSection/imageCarousel'
 const Homepage = () => {
   return (
     <>
     <Section1/>
     <Section2/>
-    <Section3/>
+    <Section3 imageCarousel={imageCarousel}/>
      
     </>
   )
