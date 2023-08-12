8dbaeacc7b2f94566ea45dc1d599bd4b01fc46fe
fierylion <mawalladaniel2018@gmail.com>
2023-08-12 20:35:17 +0300

    left contract intergration and graphs drawing

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,3 +1,6 @@
 import axios from 'axios'
-const api = axios.create({ baseURL: 'http://localhost:5000/' })
+const dev = false
+const api = axios.create({
+  baseURL: dev ? 'http://localhost:5000/' : 'https://aidreach.onrender.com',
+})
 export default api
